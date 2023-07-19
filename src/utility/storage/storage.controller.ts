88940a3e6ed35915729d0de77d195bded6bad491
Logan <clothe09986@gmail.com>
2023-07-19 10:59:02 +0800

    feat: s3 multipart upload

--- src/utility/storage/storage.controller.ts
+++ src/utility/storage/storage.controller.ts
@@ -2,21 +2,67 @@ import { Body, Controller, Post } from '@nestjs/common';
 
 import { StorageService } from './storage.service';
 import { UploadDTO } from './storage.dto';
+import { CompletedMultipartUpload } from '@aws-sdk/client-s3';
 
 @Controller({
   path: 'storage',
   version: ['2'],
 })
 export class StorageController {
-  constructor(
-    private readonly storageService: StorageService,
-  ) {}
+  constructor(private readonly storageService: StorageService) {}
 
   @Post('storage/upload')
-  uploadFileToStorageBucket(
-    @Body() body: UploadDTO,
-  ) {
+  uploadFileToStorageBucket(@Body() body: UploadDTO) {
     const { appId, fileName } = body;
     return this.storageService.getSignedUrlForUploadStorage(appId, fileName, 60);
+  }
+
+  @Post('/multipart/create')
+  async createMultipartUpload(
+    @Body()
+    body: {
+      params: {
+        Key: string;
+        ContentType: string;
+      };
+    },
+  ) {
+    const { params } = body;
+    const { UploadId } = await this.storageService.createMultipartUpload(params);
+    return {
+      uploadId: UploadId,
+    };
+  }
+
+  @Post('/multipart/sign-url')
+  async getSignedUrl(
+    @Body()
+    body: {
+      params: {
+        Key: string;
+        UploadId: string;
+        PartNumber: number;
+      };
+    },
+  ) {
+    const { params } = body;
+    const presignedUrl = await this.storageService.getSignedUrlForUploadPartStatic(params, 60);
+    return { presignedUrl };
+  }
+
+  @Post('/multipart/complete')
+  async completeMultipartUpload(
+    @Body()
+    body: {
+      params: {
+        Key: string;
+        UploadId: string;
+        MultipartUpload: CompletedMultipartUpload;
+      };
+    },
+  ) {
+    const { params } = body;
+    const result = await this.storageService.completeMultipartUpload(params);
+    return { location: result.Location };
   }
 }
