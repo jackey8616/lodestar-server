1d4775cd24c042ba65b8d32fe089135e64fe4197
Logan <clothe09986@gmail.com>
2023-07-27 15:35:25 +0800

    fix: move sign cloudfront cookies to AuthService

--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -1,26 +1,31 @@
-import { Body, Controller, Post } from "@nestjs/common";
+import { Body, Controller, Post, Res } from '@nestjs/common';
+import { Response } from 'express';
 
-import { AuthService } from "./auth.service";
-import { CrossServerTokenDTO } from "./auth.type";
+import { AuthService } from './auth.service';
+import { CrossServerTokenDTO } from './auth.type';
 
 @Controller({
   path: 'auth',
   version: ['2'],
 })
 export class AuthController {
-  constructor(
-    private readonly authService: AuthService,
-  ) {}
+  constructor(private readonly authService: AuthService) {}
 
   @Post('token')
-  async generateCrossServerToken(
-    @Body() body: CrossServerTokenDTO,
-  ) {
+  async generateCrossServerToken(@Body() body: CrossServerTokenDTO) {
     const authToken = await this.authService.generateCrossServerToken(body);
     return {
       code: 'SUCCESS',
       message: 'get auth token successfully',
       result: { authToken },
     };
+  }
+
+  @Post('/sign-cloudfront-cookies') signCookie(@Res() res: Response, @Body() body: { url: string }) {
+    const cookies = this.authService.signCloudfrontCookies(body.url);
+    res.cookie('CloudFront-Expires', cookies['CloudFront-Expires']);
+    res.cookie('CloudFront-Key-Pair-Id', cookies['CloudFront-Key-Pair-Id']);
+    res.cookie('CloudFront-Signature', cookies['CloudFront-Signature']);
+    res.send('success set cookie');
   }
 }
--- src/media/media.module.ts
+++ src/media/media.module.ts
@@ -9,10 +9,9 @@ import { VideoService } from './video/video.service';
 import { VideoController } from './video/video.controller';
 import { MediaInfrastructure } from './media.infra';
 import { MediaService } from './media.service';
-import { MediaController } from './media.controller';
 
 @Module({
-  controllers: [VideoController, MediaController],
+  controllers: [VideoController],
   providers: [
     Logger,
     AudioService,
